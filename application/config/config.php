7a8544f8c0169fe8ba8dcea91fbefa4441cc0bd7
Pradeep Kumar <pradeep0802@gmail.com>
2015-05-13 13:05:51 +0530

    First Commit for Login Application

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url']	= '/ci_registration_and_login/';
 
 /*
 |--------------------------------------------------------------------------
